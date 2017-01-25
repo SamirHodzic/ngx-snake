ab0c95e73f4a220d3041274013f8425bf6fa6822
SamirHodzic <samir.sgd@gmail.com>
2017-01-25 13:59:14 +0100

    - init ngx-snake

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # ngx-snake
-Snake arcade game implemented in Angular 2
+
+[![npm version](https://badge.fury.io/js/ngx-snake.svg)](https://badge.fury.io/js/ngx-snake)
+
+ngx-snake is simple Snake arcade game implemented in Angular 2 (latest 2.4.4)
+
+#### Demo: https://samirhodzic.github.io/ngx-snake/ (Updated: 24/01/2017)
+
+#### Controls
+
+UP, DOWN, LEFT, RIGHT - to control snake
+
+## Setup
+
+Install the npm packages described in the package.json (Angular-cli included):
+
+```bash
+$ npm install
+```
+Transpile typescript into javascript, host the app and monitor the changes: 
+
+```bash
+$ ng serve
+```
+
+Visit http://localhost:4200 and play!
---
+++ src/app/app.component.ts
@@ -0,0 +1,199 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'ngx-snake',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css'],
+	host: {
+		'(document:keydown)': 'handleKeyboardEvents($event)'
+	}
+})
+export class AppComponent implements OnInit {
+	public BOARD_SIZE = 18;
+	public board = [];
+	public interval: any;
+	public tempDirection: any;
+	public isGameOver: any;
+	public gameStarted = false;
+	private score = 0;
+
+	public CONTROLS = {
+		LEFT: 37,
+		UP: 38,
+		RIGHT: 39,
+		DOWN: 40
+	};
+
+	public COLORS = {
+		GAME_OVER: '#D24D57',
+		FRUIT: '#EC644B',
+		HEAD: '#336E7B',
+		BODY: '#C8F7C5',
+		BOARD: '#86B5BD'
+	};
+
+	private snake = {
+		direction: this.CONTROLS.LEFT,
+		parts: [
+			{
+				x: -1,
+				y: -1
+			}
+		]
+	};
+
+	private fruit = {
+		x: -1,
+		y: -1
+	};
+
+	ngOnInit() {
+		this.setBoard();
+	}
+
+	handleKeyboardEvents(e: KeyboardEvent) {
+		if (e.keyCode == this.CONTROLS.LEFT && this.snake.direction !== this.CONTROLS.RIGHT) {
+			this.tempDirection = this.CONTROLS.LEFT;
+		} else if (e.keyCode == this.CONTROLS.UP && this.snake.direction !== this.CONTROLS.DOWN) {
+			this.tempDirection = this.CONTROLS.UP;
+		} else if (e.keyCode == this.CONTROLS.RIGHT && this.snake.direction !== this.CONTROLS.LEFT) {
+			this.tempDirection = this.CONTROLS.RIGHT;
+		} else if (e.keyCode == this.CONTROLS.DOWN && this.snake.direction !== this.CONTROLS.UP) {
+			this.tempDirection = this.CONTROLS.DOWN;
+		}
+	}
+
+	setColors(col: number, row: number): any {
+		if (this.isGameOver) {
+			return this.COLORS.GAME_OVER;
+		} else if (this.fruit.x == row && this.fruit.y == col) {
+			return this.COLORS.FRUIT;
+		} else if (this.snake.parts[0].x == row && this.snake.parts[0].y == col) {
+			return this.COLORS.HEAD;
+		} else if (this.board[col][row] === true) {
+			return this.COLORS.BODY;
+		}
+
+		return this.COLORS.BOARD;
+	};
+
+	updatePositions(): void {
+		let newHead = this.repositionHead();
+		let me = this;
+
+		if (this.boardCollision(newHead) || this.selfCollision(newHead)) {
+			return this.gameOver();
+		} else if (this.fruitCollision(newHead)) {
+			this.eatFruit();
+		}
+
+		let oldTail = this.snake.parts.pop();
+		this.board[oldTail.y][oldTail.x] = false;
+
+		this.snake.parts.unshift(newHead);
+		this.board[newHead.y][newHead.x] = true;
+
+		this.snake.direction = this.tempDirection;
+
+		setTimeout(function () {
+			me.updatePositions();
+		}, this.interval);
+	}
+
+	repositionHead(): any {
+		let newHead = Object.assign({}, this.snake.parts[0]);
+
+		if (this.tempDirection === this.CONTROLS.LEFT) {
+			newHead.x -= 1;
+		} else if (this.tempDirection === this.CONTROLS.RIGHT) {
+			newHead.x += 1;
+		} else if (this.tempDirection === this.CONTROLS.UP) {
+			newHead.y -= 1;
+		} else if (this.tempDirection === this.CONTROLS.DOWN) {
+			newHead.y += 1;
+		}
+		return newHead;
+	}
+
+	boardCollision(part: any): any {
+		return part.x === this.BOARD_SIZE || part.x === -1 || part.y === this.BOARD_SIZE || part.y === -1;
+	}
+
+	selfCollision(part: any): any {
+		return this.board[part.y][part.x] === true;
+	}
+
+	fruitCollision(part: any): any {
+		return part.x === this.fruit.x && part.y === this.fruit.y;
+	}
+
+	resetFruit(): void {
+		var x = Math.floor(Math.random() * this.BOARD_SIZE);
+		var y = Math.floor(Math.random() * this.BOARD_SIZE);
+
+		if (this.board[y][x] === true) {
+			return this.resetFruit();
+		}
+
+		this.fruit = { 
+			x: x, 
+			y: y 
+		};
+	}
+
+	eatFruit(): void {
+		this.score++;
+
+		let tail = Object.assign({}, this.snake.parts[this.snake.parts.length - 1]);
+
+		this.snake.parts.push(tail);
+		this.resetFruit();
+
+		if (this.score % 5 === 0) {
+			this.interval -= 15;
+		}
+	}
+
+	gameOver(): void {
+		this.isGameOver = true;
+		this.gameStarted = false;
+		let me = this;
+
+		setTimeout(function () {
+			me.isGameOver = false;
+		}, 500);
+
+		this.setBoard();
+	}
+
+	setBoard(): void {
+		this.board = [];
+
+		for (var i = 0; i < this.BOARD_SIZE; i++) {
+			this.board[i] = [];
+			for (var j = 0; j < this.BOARD_SIZE; j++) {
+				this.board[i][j] = false;
+			}
+		}
+	}
+
+	newGame(): void {
+		if(this.gameStarted) return;
+		this.gameStarted = true;
+		this.score = 0;
+		this.tempDirection = this.CONTROLS.LEFT;
+		this.isGameOver = false;
+		this.interval = 150;
+		this.snake = { 
+			direction: this.CONTROLS.LEFT, 
+			parts: [] 
+		};
+
+		for (var i = 0; i < 3; i++) {
+			this.snake.parts.push({ x: 10 + i, y: 10 });
+		}
+
+		this.resetFruit();
+		this.updatePositions();
+	}
+}
