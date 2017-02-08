0bbb86194bc05bfed9a0d6444046ae6eed820e83
SamirHodzic <samir.sgd@gmail.com>
2017-02-08 14:34:07 +0100

    - update demo with modes selection

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import './polyfills.ts';
-
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { enableProdMode } from '@angular/core';
-import { AppModule } from './app/app.module';
-
-enableProdMode();
-
-platformBrowserDynamic().bootstrapModule(AppModule);
